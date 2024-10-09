6925d50f7127d836b46d006723b973acc2a10748
Mrkva77 <156481047+Mrkva77@users.noreply.github.com>
2024-10-09 09:26:03 +0200

    Add files via upload

---
+++ Automobil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLASA_ARRAYLIST
+{
+    internal class Class1
+    {
+        private string text;
+
+        public Class1(string text)
+        {
+            this.text = text;
+        }
+
+        class Automobil
+
+        {
+
+            private string marka;
+
+            public Automobil(string m)
+
+            {
+
+                marka = m;
+
+            }
+
+            public string DajMarku()
+
+            {
+
+                return marka;
+
+            }
+
+        }
+    }
+}
